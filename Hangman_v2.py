b0b2f2387881da8d039d6dc301d493eddd045d5e
Omar <pi@raspberrypi>
2020-01-15 22:10:53 -0600

    Made this because I saw another way to do it and I don't think it's possible the first way?

---
+++ Hangman_v2.py
@@ -0,0 +1,25 @@
+import urllib2
+import random
+import time
+import sys
+word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-ty$
+response = urllib2.urlopen(word_site)
+txt = response.read()
+#Words is a list with strings in it
+lstwords = txt.splitlines()
+answer = random.choice(lstwords).lower()
+
+
+#print answer
+def makeitadash(x):
+        x = '_ ' * len(x)
+        return x
+#ANSWER IS THE WORD YOU ARE GUESSING FOR
+#Start game
+question = raw_input("Hello, welcome to hangman. What's your name?: ")
+time.sleep(1)
+print "Oh, hi %s" % (question)
+time.sleep(1)
+#Ready to start or not
+rdornot = raw_input("Are you ready to start playing hangman?: ")
+#if rdornot.lower() == "yes":
